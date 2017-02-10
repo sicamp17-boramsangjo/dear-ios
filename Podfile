1ee8c69613c14170baa5278fac43f6943b84fa25
kyungtaek <kyungtaek.lim@gmail.com>
2017-02-11 00:36:03 +0900

    setup fastlane via fabric

--- Podfile
+++ Podfile
@@ -5,8 +5,9 @@ target 'dear' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 
-pod 'Fabric'	
-pod 'Digits'
+    pod 'Fabric'
+    pod 'Digits'
+    pod 'Crashlytics'
   # Pods for dear
 
   target 'dearTests' do
--- dear/AppDelegate.swift
+++ dear/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Fabric
 import DigitsKit
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        Fabric.with([Digits.self])
+        Fabric.with([Digits.self, Crashlytics.self])
         
         return true
     }
